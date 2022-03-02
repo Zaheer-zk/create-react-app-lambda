d2367487e7e8262265e869c144284d4b9f7b82d2
Zaheer khan <83355375+Zaheer-zk@users.noreply.github.com>
2022-03-02 12:29:40 +0530

    Add files of t and j

---
+++ build/precache-manifest.b084f230745847c204a7748400c72926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f25c2895070694bcbc476daa079a67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1768d0dd7ebf8d6f3e79",
+    "url": "/static/css/main.a02e3118.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1768d0dd7ebf8d6f3e79",
+    "url": "/static/js/main.8df20926.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
